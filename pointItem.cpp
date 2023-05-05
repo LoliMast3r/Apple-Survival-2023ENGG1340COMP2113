2f2616d31711c3a61721c3d1174463145cc82fb8
Kelvin wong <87235255+dwtse@users.noreply.github.com>
2023-05-05 13:17:11 +0800

    Update pointItem.cpp

--- pointItem.cpp
+++ pointItem.cpp
@@ -14,25 +14,44 @@ int currentSec(){
  
     std::chrono::duration<double> elapsed_seconds = end-start;
     std::time_t end_time = std::chrono::system_clock::to_time_t(end);
-    return elapsed_seconds
 }
+
+//x== width, y=height
 
 int locationGenerator(){
-      for(int i = 25; i < 50; i++){
+      for(int i = 0; i < 50; i++){
         int x = rand() % N;
+      }
+      for(int i = 0; i < 50; i++){
         int y = rand() % N;
-
-        std::cout << "x:" << x << " y:" << y << "\n\n";
-    }
+      }
 }
 
-
-
-void printItem(){
+void printItem(int playerx, int playery, int snakex, int snakey, int item_picked){
   srand(time(0));
-  while (currentSec()/10)==0{
-  
-  
+  while{gameOver!==True){
+      int item_number=0;
+      int sec = currentSec().elapsed_seconds;
+      if (sec/10)==0 and (item_number==0){
+          item_number+=1;
+          int location_x=locationGenerator().x;
+          int location_y=locationGenerator().y;
+          mvwprintw(win,location_y,location_x,3);
+          if(playerx==location_x) and (playery==location_y){
+              item_number-=1;
+              item_picked+=1;
+              mvwprintw(win,location_y,location_x," ");
+          }
+          elif(snakex==location_x) and (snakey==location_y){
+              item_number-=1;
+              mvwprintw(win,location_y,location_x," ");
+          }
+      }
+      if gameOver==True{
+        return item_picked;
+      break
+      }
   }
-  
-  }
+          
+          
+       
